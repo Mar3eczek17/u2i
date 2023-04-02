68f1ac35a988e33588e80639c11ccb10b202ad9e
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2023-04-02 15:33:29 +0200

    1.1. For loops

--- Chapter1.py
+++ Chapter1.py
@@ -1,3 +1,4 @@
+# 1.1. For loops
 factorial = 1
 # n = int(input("Enter the number (positive integer) n: "))  # factorial from 5
 n = 5  # factorial from 5
@@ -23,3 +24,26 @@ for i in range(n, 0, -1):
     for j in range(2 * i - 1):
         print('*', end=' ')
     print()
+print('#################################')
+# 1.2. While loops
+result = 0
+while n > 0:
+    n = n // 10
+    result += 1
+print(result)
+print('---------------------------------')
+a = 0
+b = 1
+n = 13
+while a <= n:
+    print(a)
+    c = a +b
+    a = b
+    b = c
+print('#################################')
+# 1.3. Looping over collections of values
+days = {'mon': 'Monday', 'tue': 'Tuesday', 'wed': 'Wednesday', 'thu': 'Thursday', 'fri': 'Friday',
+'sat': 'Saturday', 'sun': 'Sunday'}
+
+for day in days:
+    print(day, 'stands for', days[day])
